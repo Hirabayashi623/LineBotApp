aad17b87c0d10d3d50baad7bec9b38cd5ac0b702
Hirabayashi623 <ryo662233@gmail.com>
2018-06-03 22:25:57 +0900

    auto commit

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 
+# vendorフォルダ配下のライブラリを自動で読込
 require_once __DIR__ . '/vendor/autoload.php';
 
+# CurlHTTPClientのインスタンス化
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+# LineBotのインスタンス化
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
 
 $signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
@@ -27,7 +30,20 @@ foreach ($events as $event) {
     error_log('Non text message has come');
     continue;
   }
-  $bot->replyText($event->getReplyToken(), $event->getText());
+  ############################################
+  ### リプライトークンを取得
+  ############################################
+  $token = $event->getReplyToken();
+  ############################################
+  ### 受信したテキストを取得
+  ############################################
+  $recv_text = $event->getText();
+  ############################################
+  ### bot,replyText：テキストを送信する
+  ### 第一引数：リプライトークン（取得から数十秒間しか使えない）
+  ### 第二引数：送信するテキスト
+  ############################################
+  $bot->replyText($token, $recv_text);
 }
 
  ?>
